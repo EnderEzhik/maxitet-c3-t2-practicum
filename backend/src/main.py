684ef9f684e3a5cf13dc88b5d8de7ebacae3415d
EnderEzhik <Achernov2007@gmail.com>
2026-04-12 21:40:48 +0300

    Обновлен README.md. Обновлен CORS.

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,7 +7,8 @@ from src.routes import mods, versions, categories
 app = FastAPI(title="EasyMods")
 
 origins = [
-    "http://127.0.0.1:5500"
+    # "http://127.0.0.1:5500"
+    "*"
 ]
 
 app.add_middleware(
