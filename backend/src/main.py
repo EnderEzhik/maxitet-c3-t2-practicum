ecd2bab675c8d59dd9a455e888796c01b1d91cdb
EnderEzhik <Achernov2007@gmail.com>
2026-04-12 16:22:27 +0300

    backend:     add: добавлены временные настройки CORS для доступа из статических файлов.

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routes import mods, versions, categories
 
 
 app = FastAPI(title="EasyMods")
+
+origins = [
+    "http://127.0.0.1:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(mods.router)
 app.include_router(versions.router)
